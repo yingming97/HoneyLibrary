d586fd8ba7500a9ee8511f464608bc1ff52e077d
vumanhtu <tu.vumanh@tapbi.vn>
2022-10-20 14:15:13 +0700

    sua nhan vien successfully

--- app/src/main/java/pham/hien/honeylibrary/View/Tab/Option/Activity/SuaNhanVienActivity.kt
+++ app/src/main/java/pham/hien/honeylibrary/View/Tab/Option/Activity/SuaNhanVienActivity.kt
@@ -1,22 +1,56 @@
 package pham.hien.honeylibrary.View.Tab.Option.Activity
 
+import android.util.Log
+import android.util.Patterns
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.RadioButton
+import android.widget.TextView
+import pham.hien.honeylibrary.FireBase.Auth.LoginRegisterAuth
+import pham.hien.honeylibrary.FireBase.FireStore.UserDAO
+import pham.hien.honeylibrary.Model.UserModel
 import pham.hien.honeylibrary.R
+import pham.hien.honeylibrary.Utils.Constant
 import pham.hien.honeylibrary.View.Base.BaseActivity
+import pham.yingming.honeylibrary.Dialog.FailDialog
+import pham.yingming.honeylibrary.Dialog.SuccessDialog
+import java.util.regex.Pattern
 
 class SuaNhanVienActivity : BaseActivity() {
+    private lateinit var backThemNhanVien: ImageView
+    private lateinit var suahoTen: EditText
+    private lateinit var suaemail: EditText
+    private lateinit var suadiaChi: EditText
+    private lateinit var suasdt: EditText
+    private lateinit var suathuThu: RadioButton
+    private lateinit var suaquanLy: RadioButton
+    private lateinit var sua: TextView
+    private lateinit var user: UserModel
+    private lateinit var userModel: ArrayList<UserModel>
+    private lateinit var arrUser: List<UserModel>
+
     private lateinit var backSuaNhanVien: ImageView
     override fun getLayout(): Int {
         return R.layout.activity_sua_nhan_vien
     }
 
     override fun initView() {
+        suahoTen = findViewById(R.id.ed_suaHotenNhanvien)
+        suaemail = findViewById(R.id.ed_suaEmailNhanVien)
+        suadiaChi = findViewById(R.id.ed_suaDiachiNhanVien)
+        suasdt = findViewById(R.id.ed_suaSdtNhanVien)
+        suathuThu = findViewById(R.id.rd_suaThuthu)
+        suaquanLy = findViewById(R.id.rdQuanLy)
+        sua = findViewById(R.id.tv_suaNhanvien)
         backSuaNhanVien = findViewById(R.id.imb_backSuaNhanVien)
     }
 
     override fun initListener() {
         backSuaNhanVien.setOnClickListener(this)
         backSuaNV()
+        sua.setOnClickListener{
+            suaNhanVien()
+        }
     }
 
     override fun initViewModel() {
@@ -28,7 +62,10 @@ class SuaNhanVienActivity : BaseActivity() {
     }
 
     override fun initDataDefault() {
-
+        UserDAO().getListUser {
+            arrUser = it
+            user = UserModel()
+        }
     }
 
     private fun backSuaNV() {
@@ -37,4 +74,80 @@ class SuaNhanVienActivity : BaseActivity() {
         }
     }
 
+    private fun suaNhanVien() {
+        var hoten = suahoTen.text.toString()
+        var mail = suaemail.text.toString()
+        var diachi = suadiaChi.text.toString()
+        var sdt = suasdt.text.toString()
+        var thuthu: Int
+        if (suathuThu.isChecked) {
+            thuthu = Constant.QUYEN.THU_THU
+            Log.e("tuvm", "check$thuthu")
+        } else {
+            thuthu = Constant.QUYEN.ADMIN
+        }
+        checkForm(hoten, mail, diachi, sdt, thuthu) { check, user, sdt ->
+            if (check) {
+//                LoginRegisterAuth().registerNewAccount(this, user!!, sdt) { checks, users ->
+//                    if (checks) {
+//                        UserDAO().addUser(this, users)
+//                    }
+//                }
+                SuccessDialog(this,"Sửa Thành Công","").show()
+            }
+        }
+    }
+
+    private fun checkForm(
+        hoten: String,
+        email: String,
+        diachi: String,
+        sdt: String,
+        quyen: Int,
+        callback: (Boolean, UserModel?, String) -> Unit
+    ) {
+        val phonePattern = "(84|0[3|5|7|8|9])+([0-9]{8,9})\\b"
+        var title = ""
+        var haveError = false
+        if (hoten.isNullOrEmpty()) {
+            title += "\nTên không được bỏ trống"
+            haveError = true
+        } else if (email.isNullOrEmpty()) {
+            title += "\nEmail không được trống"
+            haveError = true
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            title += "\nEmail sai định dạng"
+            haveError = true
+        } else if (diachi.isNullOrEmpty()) {
+            title += "\nĐịa chỉ không được bỏ trống"
+            haveError = true
+        } else if (diachi.length!! < 6) {
+            title += "\nĐịa chỉ không dưới 6 ký tự"
+            haveError = true
+        } else if (sdt.isNullOrEmpty()) {
+            for (user in arrUser) {
+                if (user.sdt == sdt) {
+                    title += "\n SDT đã tồn tại"
+                    haveError = true
+                    break
+                }
+            }
+            title += "\nSố điện thoại không được bỏ trống"
+            haveError = true
+
+        } else if (!Pattern.compile(phonePattern).matcher(sdt).matches()) {
+            title += "\nSai định dạng số điện thoại"
+            haveError = true
+        }
+
+
+        if (haveError) {
+            callback(false, null, sdt)
+            FailDialog(this, "Thêm Thất Bại", title).show()
+        } else {
+            val userModel =
+                UserModel(arrUser[arrUser.size - 1].userId, "", quyen, hoten, email, sdt, diachi)
+            callback(true, userModel, sdt)
+        }
+    }
 }
